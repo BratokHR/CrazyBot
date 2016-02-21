e184fdf02de6ccdad61539c073468d6c3db82247
BratokHR <hripa4eff@yandex.ru>
2016-02-21 17:33:20 +0300

    Upd

--- hook.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-class chook
-{
-public:
-	void MemoryWrite(void *adr, void *ptr, int size);
-	BOOL bCompare(const BYTE *pData, const BYTE *bMask, const char *szMask);
-	DWORD FindPattern(DWORD dwAddress, DWORD dwLen, BYTE *bMask, char *szMask);
-};
--- main.h
+++
@@ -1,9 +0,0 @@
-#define Thread(function) CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)function, NULL, NULL, NULL)
-#define OFFSET_GFX_ENDFRAME 0x004C5A40
-#define	CG_OBITUARY 0x004D9ED0
-
-extern  void		(*orig_CG_Obituary)			( );
-void                HooK_CG_Obituary            ( );
-
-extern DWORD			GFX_MODULE_Base;
-extern cMenuManager		m;
