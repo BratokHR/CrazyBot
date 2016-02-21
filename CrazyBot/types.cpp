1fb0f34566b21ca3f057d33e89e126b52f7e828a
BratokHR <hripa4eff@yandex.ru>
2016-02-21 21:41:18 +0300

    Update types.cpp

--- CrazyBot/types.cpp
+++ CrazyBot/types.cpp
@@ -22,31 +22,6 @@ void crazybot::InitVersion()
 		bot.cg_entities		= (centity_t *)		0x0146DA00;
 		bot.input		= (input_t *)		0x00924864;
 		bot.OpenConsole		= (xcommand_t)		0x00403820;
-<<<<<<< HEAD
-		bot.ping			= (int *)			0x008DC234;
-		bot.viewangles		= (float *)			0x008E4824;
-
-		bot.gfx_endframe	=					0x004C5A40;
-		bot.obituary		=					0x004D9ED0;
-
-		bot.trace			=					0x004D8170;
-		bot.lerptag			=					0x004C6AE0;
-		bot.gethandle		=					0x00433DB0;
-		bot.gettagnumber	=					0x00474560;
-
-		bot.MenuOpen		=					0x00461C6F;
-		bot.NoRecoil		=					0x004D0F15;
-		bot.Wallhack		=					0x004D631E;
-		bot.Attack			=					0x00407180;
-		bot.Fov_x			=					0x00646488;
-
-		bot.CvarUnlocker1	=					0x00435840;
-		bot.CvarUnlocker2	=					0x0043585F;
-		bot.CvarUnlocker3	=					0x00435890;
-
-		bot.inGame			= (bool *)			0x00C1E26C;
-		bot.inMenu			= (int *)			0x008DC20C;
-=======
 		bot.ping		= (int *)		0x008DC234;
 		bot.viewangles		= (float *)		0x008E4824;
 
@@ -67,7 +42,9 @@ void crazybot::InitVersion()
 		bot.CvarUnlocker1	=			0x00435840;
 		bot.CvarUnlocker2	=			0x0043585F;
 		bot.CvarUnlocker3	=			0x00435890;
->>>>>>> origin/master
+
+		bot.inGame		= (bool *)		0x00C1E26C;
+		bot.inMenu		= (int *)		0x008DC20C;
 	}
 	else if (strcmp(version, "1.2") == 0)
 	{
@@ -77,31 +54,6 @@ void crazybot::InitVersion()
 		bot.cg_entities		= (centity_t *)		0x0150EA80;
 		bot.input		= (input_t *)		0x00997E2C;
 		bot.OpenConsole		= (xcommand_t)		0x004039A0;
-<<<<<<< HEAD
-		bot.ping			= (int *)			0x0093367C;
-		bot.viewangles		= (float *)			0x00957DEC;
-
-		bot.gfx_endframe	=					0x004CBC30;
-		bot.obituary		=					0x004E0380;
-
-		bot.trace			=					0x004DE620;
-		bot.lerptag			=					0x004CCCD0;
-		bot.gethandle		=					0x00435660;
-		bot.gettagnumber	=					0x004774C0;
-
-		bot.MenuOpen		=					0x0046498F;
-		bot.NoRecoil		=					0x004D70A5;
-		bot.Wallhack		=					0x004DC79E;
-		bot.Attack			=					0x00407B10;
-		bot.Fov_x			=					0x009839D4;
-
-		bot.CvarUnlocker1	=					0x00437150;
-		bot.CvarUnlocker2	=					0x0043716F;
-		bot.CvarUnlocker3	=					0x004371A0;
-
-		bot.inGame			= (bool *)			0x00CA3144;
-		bot.inMenu			= (int *)			0x00933654;
-=======
 		bot.ping		= (int *)		0x0093367C;
 		bot.viewangles		= (float *)		0x00957DEC;
 
@@ -122,7 +74,9 @@ void crazybot::InitVersion()
 		bot.CvarUnlocker1	=			0x00437150;
 		bot.CvarUnlocker2	=			0x0043716F;
 		bot.CvarUnlocker3	=			0x004371A0;
->>>>>>> origin/master
+
+		bot.inGame		= (bool *)		0x00CA3144;
+		bot.inMenu		= (int *)		0x00933654;
 	}
 	else 
 	{
@@ -132,31 +86,6 @@ void crazybot::InitVersion()
 		bot.cg_entities		= (centity_t *)		0x015E2A80;
 		bot.input		= (input_t *)		0x009CFE2C;
 		bot.OpenConsole		= (xcommand_t)		0x00403910;
-<<<<<<< HEAD
-		bot.ping			= (int *)			0x0096B67C;
-		bot.viewangles		= (float *)			0x0098FDEC;
-
-		bot.gfx_endframe	=					0x004CBCA0;
-		bot.obituary		=					0x004E03F0;
-
-		bot.trace			=					0x004DE690;
-		bot.lerptag			=					0x004CCD40;
-		bot.gethandle		=					0x004356B0;
-		bot.gettagnumber	=					0x00477500;
-
-		bot.MenuOpen		=					0x004649CF;
-		bot.NoRecoil		=					0x004D7115;
-		bot.Wallhack		=					0x004DC80E;
-		bot.Attack			=					0x00407A90;
-		bot.Fov_x			=					0x009839D4;
-
-		bot.CvarUnlocker1	=					0x004371A0;
-		bot.CvarUnlocker2	=					0x004371BF;
-		bot.CvarUnlocker3	=					0x004371DF;
-		
-		bot.inGame			= (bool *)			0x00D77144;
-		bot.inMenu			= (int *)			0x0096B654;
-=======
 		bot.ping		= (int *)		0x0096B67C;
 		bot.viewangles		= (float *)		0x0098FDEC;
 
@@ -177,7 +106,9 @@ void crazybot::InitVersion()
 		bot.CvarUnlocker1	=			0x004371A0;
 		bot.CvarUnlocker2	=			0x004371BF;
 		bot.CvarUnlocker3	=			0x004371DF;
->>>>>>> origin/master
+		
+		bot.inGame		= (bool *)		0x00D77144;
+		bot.inMenu		= (int *)		0x0096B654;
 	}
 
 	bot.InitSettings = false;
